eb1d588f06850aa87a3d480f4ba8e59eb96b80d6
Carl Fee <15cfee@gmail.com>
2019-04-13 20:19:06 -0600

    vue but not able to test

--- pkg/http/web/app/src/main.js
+++ pkg/http/web/app/src/main.js
@@ -1,9 +1,16 @@
+import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
+import router from './routes'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV == 'production';
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
